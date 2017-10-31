738e1c14efc46225e985717265f6402f9c200129
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2017-10-31 08:42:33 -0200

    TRABALHO PRONTO!!!

--- arduino.ino
+++ arduino.ino
@@ -14,8 +14,9 @@ int led12 = 2;
 int led13 = 1;
 
 char padrao_atual = 'A';
-int contador_a = 0;
-int contador_b = 0;
+int quantidade_padroes = 4;
+int mudancas_de_cada_padrao[4];
+int mudancas_de_padrao = 0;
 int valor_recebido = 0;
 
 void setup() 
@@ -33,7 +34,13 @@ void setup()
 	pinMode(led10, OUTPUT);     
 	pinMode(led11, OUTPUT);     
 	pinMode(led12, OUTPUT); 
-        pinMode(led13, OUTPUT); 
+	pinMode(led13, OUTPUT); 
+
+	int i;
+	for (i = 0; i < quantidade_padroes; i = i + 1) 
+	{
+		mudancas_de_cada_padrao[i] = 0;
+	}
 }
 
 void padrao_a() 
@@ -61,15 +68,51 @@ void padrao_b()
 	digitalWrite(led4, HIGH);
 	delay(1000);
 	digitalWrite(led4, LOW);
-        digitalWrite(led5, HIGH);
-        delay(1000);
-        digitalWrite(led5, LOW);
-        digitalWrite(led6, HIGH);
-        delay(1000);
-        digitalWrite(led6, LOW);
-        digitalWrite(led7, HIGH);
-        delay(1000);
-        digitalWrite(led7, LOW);
+	digitalWrite(led5, HIGH);
+	delay(1000);
+	digitalWrite(led5, LOW);
+	digitalWrite(led6, HIGH);
+	delay(1000);
+	digitalWrite(led6, LOW);
+	digitalWrite(led7, HIGH);
+	delay(1000);
+	digitalWrite(led7, LOW);
+}
+
+void padrao_c() 
+{
+	digitalWrite(led4, HIGH);
+	delay(1000);
+	digitalWrite(led4, LOW);
+	delay(1000);
+}
+
+void padrao_d() 
+{
+	digitalWrite(led4, HIGH);
+	delay(100);
+	digitalWrite(led4, LOW);
+	digitalWrite(led5, HIGH);
+	delay(100);
+	digitalWrite(led5, LOW);
+	digitalWrite(led6, HIGH);
+	delay(100);
+	digitalWrite(led6, LOW);
+	digitalWrite(led7, HIGH);
+	delay(100);
+	digitalWrite(led7, LOW);
+	digitalWrite(led8, HIGH);
+	delay(100);
+	digitalWrite(led8, LOW);
+	digitalWrite(led7, HIGH);
+	delay(100);
+	digitalWrite(led7, LOW);
+	digitalWrite(led6, HIGH);
+	delay(100);
+	digitalWrite(led6, LOW);
+	digitalWrite(led5, HIGH);
+	delay(100);
+	digitalWrite(led5, LOW);
 }
 
 void piscar_leds()
@@ -82,18 +125,25 @@ void piscar_leds()
 	{
 		padrao_b();
 	}
+	if(padrao_atual == 'C')
+	{
+		padrao_c();
+	}
+	else if(padrao_atual == 'D')
+	{
+		padrao_d();
+	}
 }
 
-void escrever_contador_no_serial()
+void escrever_mudancas_de_cada_padrao()
 {
-	if(padrao_atual == 'A')
+	int i;
+	for (i = 0; i < quantidade_padroes; i = i + 1) 
 	{
-		Serial.write(contador_a);
-	}
-	else if(padrao_atual == 'B')
-	{
-		Serial.write(contador_b);
-	}
+		Serial.write(mudancas_de_cada_padrao[i]);
+	} 
+
+	Serial.write(mudancas_de_padrao);  
 }
 
 void loop() 
@@ -107,20 +157,32 @@ void loop()
 			if(valor_recebido == 'A')
 			{
 				padrao_atual = 'A';
-				contador_a = contador_a + 1;
+				mudancas_de_cada_padrao[0] = mudancas_de_cada_padrao[0] + 1;
+				mudancas_de_padrao = mudancas_de_padrao + 1;
 			}	
 			else if(valor_recebido == 'B')
 			{
 				padrao_atual = 'B';
-				contador_b = contador_b + 1;
+				mudancas_de_cada_padrao[1] = mudancas_de_cada_padrao[1] + 1;
+				mudancas_de_padrao = mudancas_de_padrao + 1;
+			}
+			if(valor_recebido == 'C')
+			{
+				padrao_atual = 'C';
+				mudancas_de_cada_padrao[2] = mudancas_de_cada_padrao[2] + 1;
+				mudancas_de_padrao = mudancas_de_padrao + 1;
+			}	
+			else if(valor_recebido == 'D')
+			{
+				padrao_atual = 'D';
+				mudancas_de_cada_padrao[3] = mudancas_de_cada_padrao[3] + 1;
+				mudancas_de_padrao = mudancas_de_padrao + 1;
 			}
 			else if(valor_recebido == '0')
 			{
-				escrever_contador_no_serial();
+				escrever_mudancas_de_cada_padrao();
 			}
 		}
-
-		piscar_leds();
 	}
 	else
 	{
